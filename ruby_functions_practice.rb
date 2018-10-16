affb239792306c14d75de1ef368b891b03a0b794
Laura Manson <hellolauramanson@gmail.com>
2018-10-16 16:55:16 +0100

    test number 12

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -33,7 +33,27 @@ end
 def number_to_full_month_name(number_month)
   if number_month == 1
     return "January"
+  elsif number_month == 2
+    return "February"
+  elsif number_month == 3
+    return "March"
+  elsif number_month == 4
+    return "April"
+  elsif number_month == 5
+    return "May"
+  elsif number_month == 6
+    return "June"
+  elsif number_month == 7
+    return "July"
+  elsif number_month == 8
+    return "August"
+  elsif number_month == 9
+    return "September"
+  elsif number_month == 10
+    return "October"
+  elsif number_month == 11
+    return "November"
+  elsif number_month == 12
+    return "December"
   end
 end
-
-def number_to_
