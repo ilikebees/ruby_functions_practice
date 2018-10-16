8fbfa75da2aab1e423056d2de6a0a0eddc80a62f
Laura Manson <hellolauramanson@gmail.com>
2018-10-16 16:37:14 +0100

    tuesday lab

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,39 @@
+def return_10
+  return 10
+end
+
+def add(x,y)
+  return x + y
+end
+
+def subtract(y,x)
+  return y - x
+end
+
+def multiply(x, y)
+  return x * y
+end
+
+def divide(x,y)
+  return x / y
+end
+
+def length_of_string(string)
+  return string.length
+end
+
+def join_string(string_1, string_2)
+  return string_1 + string_2
+end
+
+def add_string_as_number(string_1, string_2)
+  return string_1.to_i + string_2.to_i
+end
+
+def number_to_full_month_name(number_month)
+  if number_month == 1
+    return "January"
+  end
+end
+
+def number_to_
